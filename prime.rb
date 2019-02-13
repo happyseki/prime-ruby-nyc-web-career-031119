166d57eacc8fb14a42771275e08771c2e91ad783
Elaina <elainaseki@gmail.com>
2019-02-13 19:51:41 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,10 +2,11 @@ def prime?(number)
   i = 1
   while i < number
   i += 1
-  if  number % i == 0 
+   if  number % i == 0 
     return false
+   else
+   return true
+   end
   end
-else
-  return true
 end
 # Add  code here!
